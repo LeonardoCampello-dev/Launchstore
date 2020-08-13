7387c569e17996889fcc6b8770701c1167ce5cdf
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-08-13 14:50:40 -0300

    Configuring multer

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const { Router } = require('express')
+const routes = express.Router()
+
+const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
 
-const routes = express.Router()
 
 routes.get("/", (req, res) => {
     return res.render("layout.njk")
 })
+
 routes.get("/products/create", ProductController.create)
 routes.get("/products/:id/edit", ProductController.edit)
 
-
-routes.post("/products", ProductController.post)
-routes.put("/products", ProductController.put)
+routes.post("/products", multer.array("photos", 6), ProductController.post)
+routes.put("/products", multer.array("photos", 6), ProductController.put)
 routes.delete("/products", ProductController.delete)
+
 
 // Alias
 
