3428928096f7257c41bd76fc58ab657aa2150c5c
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-09-07 11:46:37 -0300

    New route structure and new controllers

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const routes = express.Router()
+
+const HomeController = require('../app/controllers/HomeController')
+
+const products = require('./products')
+const users = require('./users')
+
+routes.use('/products', products)
+routes.use('/users', users)
+
+routes.get('/', HomeController.index)
+
+// Alias
+routes.get('/ads/create', (req, res) => {
+    return res.redirect('/products/create')
+})
+
+module.exports = routes
---
+++ src/routes/users.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const routes = express.Router()
+
+const SessionController = require('../app/controllers/SessionController')
+const UserController = require('../app/controllers/UserController')
+
+// Login/Logout -- SessionController
+
+/*
+routes.get('/login', SessionController.loginForm)
+routes.post('/login', SessionController.login)
+routes.post('/logout', SessionController.logout)
+
+// Reset password/Forgot -- SessionController
+
+routes.get('/forgot-password', SessionController.forgotForm)
+routes.get('/reset-password', SessionController.resetForm)
+routes.post('/forgot-password', SessionController.forgot)
+routes.post('/reset-password', SessionController.reset)
+
+// Users -- UserController
+
+routes.get('/register', UserController.create)
+routes.post('/register', UserController.post)
+
+routes.get('/', UserController.show)
+routes.put('/', UserController.put)
+routes.delete('/', UserController.delete)
+*/
+
+module.exports = routes
