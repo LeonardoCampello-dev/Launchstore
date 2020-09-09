f30ace61162b51c576985ae4b8c1703ff7ea77cd
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-09-09 10:17:16 -0300

    Creating and styling registration form

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,4 +16,8 @@ routes.get('/ads/create', (req, res) => {
     return res.redirect('/products/create')
 })
 
+routes.get('/accounts', (req, res) => {
+    return res.redirect('/users/register')
+})
+
 module.exports = routes
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,26 +6,25 @@ const UserController = require('../app/controllers/UserController')
 
 // Login/Logout -- SessionController
 
-/*
-routes.get('/login', SessionController.loginForm)
-routes.post('/login', SessionController.login)
-routes.post('/logout', SessionController.logout)
+// routes.get('/login', SessionController.loginForm)
+// routes.post('/login', SessionController.login)
+// routes.post('/logout', SessionController.logout)
 
-// Reset password/Forgot -- SessionController
+// // Reset password/Forgot -- SessionController
 
-routes.get('/forgot-password', SessionController.forgotForm)
-routes.get('/reset-password', SessionController.resetForm)
-routes.post('/forgot-password', SessionController.forgot)
-routes.post('/reset-password', SessionController.reset)
+// routes.get('/forgot-password', SessionController.forgotForm)
+// routes.get('/reset-password', SessionController.resetForm)
+// routes.post('/forgot-password', SessionController.forgot)
+// routes.post('/reset-password', SessionController.reset)
 
-// Users -- UserController
+// // Users -- UserController
 
 routes.get('/register', UserController.create)
-routes.post('/register', UserController.post)
+// routes.post('/register', UserController.post)
 
-routes.get('/', UserController.show)
-routes.put('/', UserController.put)
-routes.delete('/', UserController.delete)
-*/
+// routes.get('/', UserController.show)
+// routes.put('/', UserController.put)
+// routes.delete('/', UserController.delete)
+
 
 module.exports = routes
