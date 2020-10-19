8aeb333c0ef965fb69de50617b44935b83763371
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-10-19 10:03:30 -0300

    Adding items to the cart

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -16,11 +16,39 @@ const Cart = {
 
         return this
     },
-    addOne(product) { },
+    addOne(product) {
+        let inCart = this.items.find(item => item.product.id == product.id)
+
+        if (!inCart) {
+            inCart = {
+                product: {
+                    ...product,
+                    formattedPrice: formatPrice(product.price)
+                },
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0)
+            }
+
+            this.items.push(inCart)
+        }
+
+        if (inCart.quantity >= product.quantity) return this
+
+        // update product
+        inCart.quantity++
+        inCart.price = inCart.product.price * inCart.quantity
+        inCart.formattedPrice = formatPrice(inCart.price)
+
+        // update cart 
+        this.total.quantity++
+        this.total.price += inCart.product.price
+        this.total.formattedPrice = formatPrice(this.total.price)
+
+        return this
+    },
     removeOne(productId) { },
     delete(productId) { }
 }
-
-console.log(Cart.init().total)
 
 module.exports = Cart
